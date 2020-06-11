73c737fabc95d8b647b8a45de6e693703c9fae04
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-06-11 19:05:51 +0800

    [cmake/actions] Infrastructure changes for TVM
    - Use C++17
    - Add TVM as a dependency
    - Drop Tasks dependency
    - Disable packaging for now since TVM is not packaged yet
    - Drop support for GitLab CI since the compilers don't support C++17
    
    Note: this branch won't build for a while...

--- utils/build_and_install.sh
+++ utils/build_and_install.sh
@@ -870,7 +870,7 @@ check_and_clone_git_dependency()
 }
 
 # If the dependencies have already been cloned, check if the local state of the repository is clean before upgrading
-GIT_DEPENDENCIES="gabime/spdlog#v1.6.1 humanoid-path-planner/hpp-spline#v4.7.0 jrl-umi3218/SpaceVecAlg jrl-umi3218/state-observation jrl-umi3218/sch-core jrl-umi3218/RBDyn jrl-umi3218/eigen-qld jrl-umi3218/eigen-quadprog jrl-umi3218/Tasks jrl-umi3218/mc_rbdyn_urdf"
+GIT_DEPENDENCIES="gabime/spdlog#v1.6.1 humanoid-path-planner/hpp-spline#v4.7.0 jrl-umi3218/SpaceVecAlg jrl-umi3218/state-observation jrl-umi3218/sch-core jrl-umi3218/RBDyn jrl-umi3218/eigen-qld jrl-umi3218/eigen-quadprog jrl-umi3218/tvm jrl-umi3218/mc_rbdyn_urdf"
 for repo in $GIT_DEPENDENCIES; do
   check_and_clone_git_dependency $repo $SOURCE_DIR
 done
@@ -1180,7 +1180,7 @@ then
   echo_log "-- [OK] Successfully built $git_dep to $repo_dir"
 fi
 
-build_git_dependency jrl-umi3218/Tasks tasks
+build_git_dependency jrl-umi3218/tvm
 build_git_dependency jrl-umi3218/mc_rbdyn_urdf mc_rbdyn_urdf
 
 if $WITH_ROS_SUPPORT
