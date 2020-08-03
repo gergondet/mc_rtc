ecd910b5af72c95c66e8f5f0c41175733985281b
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-08-03 15:30:38 +0800

    [mc_tvm] Add OrientationFunction

--- include/mc_tvm/OrientationFunction.h
+++ include/mc_tvm/OrientationFunction.h
@@ -3,3 +3,80 @@
  */
 
 #pragma once
+
+#include <mc_tvm/api.h>
+
+#include <mc_rbdyn/fwd.h>
+
+#include <tvm/function/abstract/Function.h>
+
+namespace mc_tvm
+{
+
+/** This class implements an orientation function for a given frame */
+class MC_TVM_DLLAPI OrientationFunction : public tvm::function::abstract::Function
+{
+public:
+  SET_UPDATES(OrientationFunction, Value, Velocity, Jacobian, NormalAcceleration)
+
+  /** Constructor
+   *
+   * Set the objective to the current frame orientation
+   *
+   */
+  OrientationFunction(const mc_rbdyn::Frame & frame);
+
+  /** Set the target orientation to the current frame orientation */
+  void reset();
+
+  /** Get the current objective */
+  inline const Eigen::Matrix3d & orientation() const noexcept
+  {
+    return ori_;
+  }
+
+  /** Set the objective */
+  inline void orientation(const Eigen::Matrix3d & ori) noexcept
+  {
+    ori_ = ori;
+  }
+
+  /** Get the current objective */
+  inline const Eigen::Vector3d & refVel() const noexcept
+  {
+    return refVel_;
+  }
+
+  /** Set the objective */
+  inline void refVel(const Eigen::Vector3d & refVel) noexcept
+  {
+    refVel_ = refVel;
+  }
+
+  /** Get the current objective */
+  inline const Eigen::Vector3d & refAccel() const noexcept
+  {
+    return refAccel_;
+  }
+
+  /** Set the objective */
+  inline void refAccel(const Eigen::Vector3d & refAccel) noexcept
+  {
+    refAccel_ = refAccel;
+  }
+
+protected:
+  void updateValue();
+  void updateVelocity();
+  void updateJacobian();
+  void updateNormalAcceleration();
+
+  mc_rbdyn::ConstFramePtr frame_;
+
+  /** Target */
+  Eigen::Matrix3d ori_;
+  Eigen::Vector3d refVel_;
+  Eigen::Vector3d refAccel_;
+};
+
+} // namespace mc_tvm
--- src/mc_tvm/OrientationFunction.cpp
+++ src/mc_tvm/OrientationFunction.cpp
@@ -1,1 +1,57 @@
 #include <mc_tvm/OrientationFunction.h>
+
+#include <mc_rbdyn/Frame.h>
+#include <mc_rbdyn/Robot.h>
+
+namespace mc_tvm
+{
+
+OrientationFunction::OrientationFunction(const mc_rbdyn::Frame & frame)
+: tvm::function::abstract::Function(3), frame_(frame)
+{
+  reset();
+  // clang-format off
+  registerUpdates(Update::Value, &OrientationFunction::updateValue,
+                  Update::Velocity, &OrientationFunction::updateVelocity,
+                  Update::Jacobian, &OrientationFunction::updateJacobian,
+                  Update::NormalAcceleration, &OrientationFunction::updateNormalAcceleration);
+  // clang-format on
+  addOutputDependency<OrientationFunction>(Output::Value, Update::Value);
+  addOutputDependency<OrientationFunction>(Output::Velocity, Update::Velocity);
+  addOutputDependency<OrientationFunction>(Output::Jacobian, Update::Jacobian);
+  addOutputDependency<OrientationFunction>(Output::NormalAcceleration, Update::NormalAcceleration);
+  const auto & robot = frame_->robot();
+  addVariable(robot.q(), false);
+  addInputDependency<OrientationFunction>(Update::Value, frame_, mc_rbdyn::Frame::Output::Position);
+  addInputDependency<OrientationFunction>(Update::Velocity, frame_, mc_rbdyn::Frame::Output::Velocity);
+  addInputDependency<OrientationFunction>(Update::Jacobian, frame_, mc_rbdyn::Frame::Output::Jacobian);
+  addInputDependency<OrientationFunction>(Update::NormalAcceleration, frame_,
+                                          mc_rbdyn::Frame::Output::NormalAcceleration);
+}
+
+void OrientationFunction::reset()
+{
+  ori_ = frame_->position().rotation();
+}
+
+void OrientationFunction::updateValue()
+{
+  value_ = sva::rotationError(ori_, frame_->position().rotation());
+}
+
+void OrientationFunction::updateVelocity()
+{
+  velocity_ = frame_->velocity().angular() - refVel_;
+}
+
+void OrientationFunction::updateJacobian()
+{
+  splitJacobian(frame_->jacobian().topRows<3>(), frame_->robot().q());
+}
+
+void OrientationFunction::updateNormalAcceleration()
+{
+  normalAcceleration_ = frame_->normalAcceleration().angular();
+}
+
+} // namespace mc_tvm
