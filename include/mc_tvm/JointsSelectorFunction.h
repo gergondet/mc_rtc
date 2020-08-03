24a0ec2d93ab781c8704f4fa841dc5dd51e4a99b
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-08-03 15:24:34 +0800

    [mc_tvm] Add JointsSelectorFunction

--- include/mc_tvm/JointsSelectorFunction.h
+++ include/mc_tvm/JointsSelectorFunction.h
@@ -3,3 +3,94 @@
  */
 
 #pragma once
+
+#include <mc_tvm/api.h>
+
+#include <mc_rbdyn/fwd.h>
+
+#include <tvm/function/abstract/Function.h>
+
+namespace mc_tvm
+{
+
+/** This class implements joints' selection on a given fucntion */
+class MC_TVM_DLLAPI JointsSelectorFunction : public tvm::function::abstract::Function
+{
+public:
+  SET_UPDATES(JointsSelector, Jacobian, JDot)
+
+  /** Construct a JointsSelector from a vector of active joints
+   *
+   * \param f Function selected by this JointsSelector
+   *
+   * \param robot Robot controlled by \p f
+   *
+   * \param activeJoints Joints active in this selector
+   *
+   * \throws If \p does not depend on \p robot or any joint in activeJoints
+   * is not part of \p robot
+   */
+  static std::unique_ptr<JointsSelector> ActiveJoints(tvm::FunctionPtr f,
+                                                      const mc_rbdyn::Robot & robot,
+                                                      const std::vector<std::string> & activeJoints);
+
+  /** Construct a JointsSelector from a vector of inactive joints
+   *
+   * \param f Function selected by this JointsSelector
+   *
+   * \param robot Robot controlled by \p f
+   *
+   * \param inactiveJoints Joints not active in this selector
+   *
+   * \throws If \p does not depend on \p robot or any joint in activeJoints
+   * is not part of \p robot
+   */
+  static std::unique_ptr<JointsSelector> InactiveJoints(tvm::FunctionPtr f,
+                                                        const mc_rbdyn::Robot & robot,
+                                                        const std::vector<std::string> & inactiveJoints);
+
+  inline const Eigen::VectorXd & value() const override
+  {
+    return f_->value();
+  }
+
+  inline const Eigen::VectorXd & velocity() const override
+  {
+    return f_->velocity();
+  }
+
+  inline const Eigen::VectorXd & normalAcceleration() const override
+  {
+    return f_->normalAcceleration();
+  }
+
+protected:
+  /** Constructor
+   *
+   * \param f Function selected by this JointsSelector
+   *
+   * \param robot Robot controlled by \p f
+   *
+   * \param fbActive True if the selected joints include the floating-base
+   *
+   * \param activeIndex For regular joints selected by this JointsSelector,
+   * this iss a list of (start, size) pair corresponding to the list of
+   * joints
+   *
+   */
+  JointsSelectorFunction(tvm::FunctionPtr f,
+                         const mc_rbdyn::Robot & robot,
+                         bool fbActive,
+                         const std::vector<std::pair<Eigen::DenseIndex, Eigen::DenseIndex>> & activeIndex);
+
+protected:
+  void updateJacobian();
+  void updateJDot();
+
+  tvm::FunctionPtr f_;
+  mc_rbdyn::ConstRobotPtr robot_;
+  bool fbActive_;
+  std::vector<std::pair<Eigen::DenseIndex, Eigen::DenseIndex>> activeIndex_;
+};
+
+} // namespace mc_tvm
--- src/mc_tvm/JointsSelectorFunction.cpp
+++ src/mc_tvm/JointsSelectorFunction.cpp
@@ -1,1 +1,153 @@
+/*
+ * Copyright 2015-2020 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
 #include <mc_tvm/JointsSelectorFunction.h>
+
+#include <mc_rbdyn/Robot.h>
+
+namespace mc_tvm
+{
+
+std::unique_ptr<JointsSelectorFunction> JointsSelectorFunction::ActiveJoints(
+    tvm::FunctionPtr f,
+    const mc_rbdyn::Robot & robot,
+    const std::vector<std::string> & activeJoints)
+{
+  auto useRobotVariable = [&](const tvm::VariablePtr & v) {
+    return std::find(f->variables().begin(), f->variables().end(), v) != f->variables().end();
+  };
+  if(!useRobotVariable(robot.qJoints()) || !useRobotVariable(robot.qFloatingBase()))
+  {
+    mc_rtc::log::error_and_throw<std::runtime_error>(
+        "Cannot setup joint selector on the provided function as it doesn't use variables of {}", robot.name());
+  }
+
+  const auto & mb = robot.mb();
+
+  std::vector<std::string> joints = activeJoints;
+  std::sort(joints.begin(), joints.end(), [&mb](const std::string & lhs, const std::string & rhs) {
+    return mb.jointPosInDof(mb.jointIndexByName(lhs)) < mb.jointPosInDof(mb.jointIndexByName(rhs));
+  });
+
+  bool ffActive = joints.size() > 0 && joints[0] == mb.joint(0).name() && mb.joint(0).dof() == 6;
+  if(ffActive)
+  {
+    joints.erase(joints.begin());
+  }
+
+  Eigen::DenseIndex ffSize = mb.joint(0).dof() == 6 ? 6 : 0;
+  std::vector<std::pair<Eigen::DenseIndex, Eigen::DenseIndex>> activeIndex;
+  Eigen::DenseIndex start = 0;
+  Eigen::DenseIndex size = 0;
+  for(const auto & j : joints)
+  {
+    auto jIndex = mb.jointIndexByName(j);
+    auto jDof = mb.joint(jIndex).dof();
+    Eigen::DenseIndex pos = mb.jointPosInDof(jIndex) - ffSize;
+    if(pos != start + size)
+    {
+      if(size != 0)
+      {
+        activeIndex.push_back({start, size});
+      }
+      start = pos;
+      size = jDof;
+    }
+    else
+    {
+      size += jDof;
+    }
+  }
+  if(size != 0)
+  {
+    activeIndex.push_back({start, size});
+  }
+
+  return std::make_unique<JointsSelectorFunction>(f, robot, ffActive, activeIndex);
+}
+
+std::unique_ptr<JointsSelectorFunction> JointsSelectorFunction::InactiveJoints(
+    tvm::FunctionPtr f,
+    const mc_rbdyn::Robot & robot,
+    const std::vector<std::string> & inactiveJoints)
+{
+  std::vector<std::string> activeJoints{};
+  for(const auto & j : robot.mb().joints())
+  {
+    if(std::find_if(inactiveJoints.begin(), inactiveJoints.end(), [&j](const std::string & s) { return s == j.name(); })
+       == inactiveJoints.end())
+    {
+      activeJoints.push_back(j.name());
+    }
+  }
+  return ActiveJoints(f, robot, activeJoints);
+}
+
+JointsSelectorFunction::JointsSelectorFunction(
+    tvm::FunctionPtr f,
+    const mc_rbdyn::Robot & robot,
+    bool ffActive,
+    const std::vector<std::pair<Eigen::DenseIndex, Eigen::DenseIndex>> & activeIndex)
+: tvm::function::abstract::Function(f->imageSpace()), f_(f), robot_(robot), fbActive_(ffActive),
+  activeIndex_(activeIndex)
+{
+  addDirectDependency<JointsSelectorFunction>(Output::Value, *f_, Function::Output::Value);
+  addDirectDependency<JointsSelectorFunction>(Output::Velocity, *f_, Function::Output::Velocity);
+  addDirectDependency<JointsSelectorFunction>(Output::NormalAcceleration, *f_, Function::Output::NormalAcceleration);
+
+  registerUpdates(Update::Jacobian, &JointsSelectorFunction::updateJacobian);
+  registerUpdates(Update::JDot, &JointsSelectorFunction::updateJDot);
+
+  addOutputDependency<JointsSelectorFunction>(Output::Jacobian, Update::Jacobian);
+  addOutputDependency<JointsSelectorFunction>(Output::JDot, Update::JDot);
+
+  addInputDependency<JointsSelectorFunction>(Update::Jacobian, *f_, Function::Output::Jacobian);
+  addInputDependency<JointsSelectorFunction>(Update::JDot, *f_, Function::Output::JDot);
+
+  if(fbActive_)
+  {
+    addVariable(robot_->qFloatingBase(), f_->linearIn(*robot_->qFloatingBase()));
+    jacobian_.at(robot_->qFloatingBase().get()).setZero();
+  }
+  if(activeIndex.size())
+  {
+    addVariable(robot_->qJoints(), f_->linearIn(*robot_->qJoints()));
+    jacobian_.at(robot_->qJoints().get()).setZero();
+  }
+}
+
+void JointsSelectorFunction::updateJacobian()
+{
+  if(fbActive_)
+  {
+    jacobian_[robot_->qFloatingBase().get()] = f_->jacobian(*robot_->qFloatingBase());
+  }
+  if(activeIndex_.size())
+  {
+    const auto & jacIn = f_->jacobian(*robot_->qJoints());
+    for(const auto & p : activeIndex_)
+    {
+      jacobian_[robot_->qJoints().get()].middleCols(p.first, p.second) =
+          jacIn.block(0, p.first, jacIn.rows(), p.second);
+    }
+  }
+}
+
+void JointsSelectorFunction::updateJDot()
+{
+  if(fbActive_)
+  {
+    JDot_[robot_->qFloatingBase().get()] = f_->JDot(*robot_->qFloatingBase());
+  }
+  if(activeIndex_.size())
+  {
+    const auto & JDotIn = f_->JDot(*robot_->qJoints());
+    for(const auto & p : activeIndex_)
+    {
+      JDot_[robot_->qJoints().get()].middleCols(p.first, p.second) = JDotIn.block(0, p.first, JDotIn.rows(), p.second);
+    }
+  }
+}
+
+} // namespace mc_tvm
