fd28471d8c736a388126345d3bd810497d4f0922
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-08-13 17:31:54 +0800

    For now we cannot add contact after adding the dynamic constraint

--- src/mc_control/samples/CoM/mc_com_controller.cpp
+++ src/mc_control/samples/CoM/mc_com_controller.cpp
@@ -31,19 +31,12 @@ MCCoMController::MCCoMController(std::shared_ptr<mc_rbdyn::RobotModule> robot_mo
   }
 
   datastore().make_call("KinematicAnchorFrame::" + robot().name(), [this](const mc_rbdyn::Robot & robot) {
-    return sva::interpolate(robot.surfacePose(leftFootSurface_), robot.surfacePose(rightFootSurface_), 0.5);
+    return sva::interpolate(robot.frame(leftFootSurface_).position(), robot.frame(rightFootSurface_).position(), 0.5);
   });
-  solver().addConstraint(dynamicsConstraint_);
-  solver().addConstraint(collisionConstraint_);
-  // FIXME
-  // solver().addConstraint(compoundJointConstraint_);
 
   postureTask_->stiffness(1);
   postureTask_->weight(1);
   solver().addTask(postureTask_);
-
-  comTask_ = std::make_shared<mc_tasks::CoMTask>(robot());
-  comTask_->weight(1000);
 
   mc_rtc::log::success("CoM sample controller initialized");
 }
@@ -51,7 +44,8 @@ MCCoMController::MCCoMController(std::shared_ptr<mc_rbdyn::RobotModule> robot_mo
 void MCCoMController::reset(const ControllerResetData & reset_data)
 {
   MCController::reset(reset_data);
-  comTask_->reset();
+  comTask_ = std::make_shared<mc_tasks::CoMTask>(robot());
+  comTask_->weight(1000);
   solver().addTask(comTask_);
   if(robot().hasSurface("LFullSole") && robot().hasSurface("RFullSole"))
   {
@@ -68,6 +62,10 @@ void MCCoMController::reset(const ControllerResetData & reset_data)
     mc_rtc::log::error_and_throw<std::runtime_error>("CoM sample controller does not support this robot {}",
                                                      robot().name());
   }
+  solver().addConstraint(dynamicsConstraint_);
+  solver().addConstraint(collisionConstraint_);
+  // FIXME
+  // solver().addConstraint(compoundJointConstraint_);
 }
 
 bool MCCoMController::run()
