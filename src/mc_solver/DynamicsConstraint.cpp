ad67a90345fe3b84324997a16abb45a430d1c364
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-08-13 17:35:49 +0800

    [mc_solver] Solve issues with DynamicsConstraint
    - Forces were not correctly added due to points vector move
    - Wrong sign for the force jacobian

--- src/mc_solver/DynamicsConstraint.cpp
+++ src/mc_solver/DynamicsConstraint.cpp
@@ -29,11 +29,11 @@ void DynamicsConstraint::addToSolver(QPSolver & solver)
   auto dyn = solver.problem().add(dynamic_ == 0., tvm::task_dynamics::None(), {tvm::requirements::PriorityLevel(0)});
   constraints_.push_back(dyn);
   auto cstr = solver.problem().constraint(dyn.get());
-  auto tl = solver.problem().add(robot_->limits().tl <= robot_->tau() <= robot_->limits().tu,
-                                 tvm::task_dynamics::None(), {tvm::requirements::PriorityLevel(0)});
-  constraints_.push_back(tl);
-  // FIXME Add torque derivative limits
   solver.problem().add(tvm::hint::Substitution(cstr, robot_->tau()));
+  auto tL = solver.problem().add(robot_->limits().tl <= robot_->tau() <= robot_->limits().tu,
+                                 tvm::task_dynamics::None(), {tvm::requirements::PriorityLevel(0)});
+  constraints_.push_back(tL);
+  // FIXME Add torque derivative limits
 }
 
 void DynamicsConstraint::removeFromSolver(QPSolver & solver)
