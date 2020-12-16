60a55984f22f33240d9415dee8d0b9975142229f
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-12-16 11:28:13 +0800

    [mc_tasks] Adapt AdmittanceTask

--- include/mc_tasks/AdmittanceTask.h
+++ include/mc_tasks/AdmittanceTask.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include <mc_filter/utils/clamp.h>
-#include <mc_tasks/SurfaceTransformTask.h>
+#include <mc_tasks/TransformTask.h>
 
 namespace mc_tasks
 {
@@ -34,19 +34,14 @@ namespace force
  * [4] https://gite.lirmm.fr/multi-contact/mc_rtc/issues/34
  *
  */
-struct MC_TASKS_DLLAPI AdmittanceTask : SurfaceTransformTask
+struct MC_TASKS_DLLAPI AdmittanceTask : TransformTask
 {
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   /*! \brief Initialize a new admittance task.
    *
-   * \param robotSurface Name of the surface frame to control, in which the
-   * desired wrench will also be expressed
-   *
-   * \param robots Robots where the task will be applied
-   *
-   * \param robotIndex Which robot among the robots
+   * \param frame Frame to control
    *
    * \param stiffness Stiffness of the underlying SurfaceTransform task
    *
@@ -56,11 +51,7 @@ public:
    * sensor attached to it
    *
    */
-  AdmittanceTask(const std::string & robotSurface,
-                 const mc_rbdyn::Robots & robots,
-                 unsigned robotIndex,
-                 double stiffness = 5.0,
-                 double weight = 1000.0);
+  AdmittanceTask(mc_rbdyn::Frame & frame, double stiffness = 5.0, double weight = 1000.0);
 
   /*! \brief Reset the task
    *
@@ -74,7 +65,7 @@ public:
   /*! \brief Get the admittance coefficients of the task
    *
    */
-  const sva::ForceVecd & admittance() const
+  const sva::ForceVecd & admittance() const noexcept
   {
     return admittance_;
   }
@@ -84,50 +75,50 @@ public:
    * \param admittance Vector of positive admittance coefficients
    *
    */
-  void admittance(const sva::ForceVecd & admittance)
+  void admittance(const sva::ForceVecd & admittance) noexcept
   {
     admittance_ = admittance;
   }
 
-  /*! \brief Get the current pose of the robot surface in the inertial frame
+  /*! \brief Get the current pose of the controlled frame in the inertial frame
    *
    */
-  sva::PTransformd surfacePose() const
+  const sva::PTransformd & pose() const noexcept
   {
-    return robots_.robot(rIndex_).surface(surfaceName).X_0_s(robots_.robot(rIndex_));
+    return frame().position();
   }
 
-  /*! \brief Get the target pose of the surface in the world frame.
+  /*! \brief Get the target pose of the controlled frame in the world frame.
    *
    */
-  sva::PTransformd targetPose()
+  const sva::PTransformd & targetPose() const noexcept
   {
     return this->target();
   }
 
-  /*! \brief Set target pose (position and orientation) of the surface in the
+  /*! \brief Set target pose (position and orientation) of the controlled frame in the
    * world frame.
    *
    * \param X_0_target Plucker transform to the target frame.
    *
    */
-  void targetPose(const sva::PTransformd & X_0_target)
+  void targetPose(const sva::PTransformd & X_0_target) noexcept
   {
     this->target(X_0_target);
   }
 
-  /*! \brief Transform from current surface pose to target.
+  /*! \brief Transform from current frame pose to target pose.
    *
    */
-  sva::PTransformd poseError()
+  sva::PTransformd poseError() noexcept
   {
-    return targetPose() * surfacePose().inv();
+    return targetPose() * pose().inv();
   }
 
-  /*! \brief Get the target wrench in the surface frame
+  /*! \brief Get the target wrench in the controlled frame
    *
    */
-  const sva::ForceVecd & targetWrench() const
+  const sva::ForceVecd & targetWrench() const noexcept
   {
     return targetWrench_;
   }
@@ -138,10 +129,9 @@ public:
    *
    * \param wrench Target wrench in world frame
    */
-  void targetWrenchW(const sva::ForceVecd & wrenchW)
+  void targetWrenchW(const sva::ForceVecd & wrenchW) noexcept
   {
-    const auto & X_0_rh = robots_.robot(rIndex_).surface(surface_.name()).X_0_s(robots_.robot(rIndex_));
-    targetWrench(X_0_rh.dualMul(wrenchW));
+    targetWrench(pose().dualMul(wrenchW));
   }
 
   /*! \brief Set the target wrench in the surface frame
@@ -149,7 +139,7 @@ public:
    * \param wrench Target wrench in the surface frame
    *
    */
-  void targetWrench(const sva::ForceVecd & wrench)
+  void targetWrench(const sva::ForceVecd & wrench) noexcept
   {
     targetWrench_ = wrench;
   }
@@ -157,18 +147,17 @@ public:
   /*! \brief Get the measured wrench in the surface frame
    *
    */
-  sva::ForceVecd measuredWrench() const
+  sva::ForceVecd measuredWrench() const noexcept
   {
-    return robots_.robot(rIndex_).surfaceWrench(surface_.name());
+    return frame().wrench();
   }
 
   /*! \brief Set the maximum translation velocity of the task */
-  void maxLinearVel(const Eigen::Vector3d & maxLinearVel)
+  void maxLinearVel(const Eigen::Vector3d & maxLinearVel) noexcept
   {
     if((maxLinearVel.array() <= 0.).any())
     {
       mc_rtc::log::error("discarding maxLinearVel update as it is not positive");
-      ;
       return;
     }
     maxLinearVel_ = maxLinearVel;
@@ -181,12 +170,11 @@ public:
   }
 
   /*! \brief Set the maximum angular velocity of the task */
-  void maxAngularVel(const Eigen::Vector3d & maxAngularVel)
+  void maxAngularVel(const Eigen::Vector3d & maxAngularVel) noexcept
   {
     if((maxAngularVel.array() <= 0.).any())
     {
       mc_rtc::log::error("discarding maxAngularVel update as it is not positive");
-      ;
       return;
     }
     maxAngularVel_ = maxAngularVel;
@@ -202,7 +190,7 @@ public:
    *
    * \param gain Gain between 0 and 1.
    */
-  void velFilterGain(double gain)
+  void velFilterGain(double gain) noexcept
   {
     velFilterGain_ = mc_filter::utils::clamp(gain, 0, 1);
   }
@@ -226,7 +214,7 @@ public:
    * control.
    *
    */
-  void refVelB(const sva::MotionVecd & velB)
+  void refVelB(const sva::MotionVecd & velB) noexcept
   {
     feedforwardVelB_ = velB;
   }
@@ -237,11 +225,8 @@ public:
 protected:
   Eigen::Vector3d maxAngularVel_ = {0.1, 0.1, 0.1}; // [rad] / [s]
   Eigen::Vector3d maxLinearVel_ = {0.1, 0.1, 0.1}; // [m] / [s]
-  const mc_rbdyn::Robots & robots_;
-  unsigned int rIndex_;
   double timestep_;
   double velFilterGain_ = 0.8; //< Gain for the low-pass filter on reference velocity [0..1]
-  const mc_rbdyn::Surface & surface_;
   sva::ForceVecd admittance_ = sva::ForceVecd(Eigen::Vector6d::Zero());
   sva::ForceVecd targetWrench_ = sva::ForceVecd(Eigen::Vector6d::Zero());
   sva::ForceVecd wrenchError_ = sva::ForceVecd(Eigen::Vector6d::Zero());
@@ -256,13 +241,13 @@ protected:
   /** Surface transform's refVelB() becomes internal to the task.
    *
    */
-  using SurfaceTransformTask::refVelB;
+  using TransformTask::refVelB;
 
   /** Surface transform's target becomes internal to the task. Its setter is
    * now targetPose().
    *
    */
-  using SurfaceTransformTask::target;
+  using TransformTask::target;
 
   /** Override addToSolver in order to get the timestep's solver automatically
    *
