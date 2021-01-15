37c5184cfc77e09c06e6620a0246b6b8b776bfc9
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-01-15 15:15:34 +0800

    [mc_rbdyn] Update CoM velocity/acceleration

--- src/mc_rbdyn/Robot.cpp
+++ src/mc_rbdyn/Robot.cpp
@@ -1374,6 +1374,8 @@ void Robot::updateKinematics()
 {
   forwardKinematics();
   com_->updateCoM();
+  com_->updateVelocity();
+  com_->updateAcceleration();
   momentum_->updateMomentum();
   for(auto & f : frames_)
   {
