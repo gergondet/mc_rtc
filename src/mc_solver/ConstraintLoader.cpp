562a0760c5e27efe551790e11bddfb111e10882e
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-04-08 17:12:12 +0800

    [mc_solver] Add a dummy contact constraint
    This avoid crashes when loading the contact constraint from YAML/JSON

--- src/mc_solver/ConstraintLoader.cpp
+++ src/mc_solver/ConstraintLoader.cpp
@@ -18,7 +18,23 @@ ConstraintLoader::storage_t & ConstraintLoader::storage()
 namespace
 {
 
+struct DummyContactConstraint : public mc_solver::Constraint
+{
+  ~DummyContactConstraint() final {}
+
+  DummyContactConstraint() : mc_solver::Constraint()
+  {
+    name_ = "ContactConstraint";
+    mc_rtc::log::warning("Contact constraint is now an empty class, we keep an empty constraint around for backward "
+                         "compatibility but you can safely remove it from your YAML/JSON configuration file");
+  }
+
+  void addToSolver(mc_solver::QPSolver &) final {}
+
+  void removeFromSolver(mc_solver::QPSolver &) final {}
+};
+
 static auto registered = mc_solver::ConstraintLoader::register_load_function(
     "contact",
-    [](mc_solver::QPSolver &, const mc_rtc::Configuration &) { return nullptr; });
-}
+    [](mc_solver::QPSolver &, const mc_rtc::Configuration &) { return std::make_shared<DummyContactConstraint>(); });
+} // namespace
