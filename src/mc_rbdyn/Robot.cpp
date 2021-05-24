63d24532c9cefaed0382541d685d702001735a9e
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-24 11:57:08 +0800

    [mc_rbdyn] Fix a bug when a base is provided

--- src/mc_rbdyn/Robot.cpp
+++ src/mc_rbdyn/Robot.cpp
@@ -117,7 +117,7 @@ Robot::Robot(make_shared_token,
 
   if(base)
   {
-    std::string baseName = bName ? mb().body(0).name() : std::string(bName.value());
+    std::string baseName = bName ? std::string(bName.value()) : mb().body(0).name();
     mb() = mbg().makeMultiBody(baseName, mb().joint(0).type() == rbd::Joint::Fixed, *base);
     mbc() = rbd::MultiBodyConfig(mb());
   }
