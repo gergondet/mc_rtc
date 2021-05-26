8b80db2b6c9a1da6311f402a0483cdc04385d6fc
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-26 16:10:54 +0800

    [tests] Test more contacts scenarios

--- tests/controllers/TestContactsController.cpp
+++ tests/controllers/TestContactsController.cpp
@@ -20,7 +20,7 @@ struct MC_CONTROL_DLLAPI TestContactsController : public MCController
 {
   TestContactsController(mc_rbdyn::RobotModulePtr rm, double dt) : MCController(rm, dt)
   {
-    solver().addConstraint(dynamicsConstraint_);
+    solver().addConstraint(kinematicsConstraint_);
     postureTask_->stiffness(1.0);
     postureTask_->weight(1.0);
     solver().addTask(postureTask_);
@@ -37,6 +37,7 @@ struct MC_CONTROL_DLLAPI TestContactsController : public MCController
   void reset(const ControllerResetData & reset_data) override
   {
     MCController::reset(reset_data);
+    qInit_ = robot().q()->value();
     comTask_->reset();
     lfTask_->reset();
     lfTask_->target(sva::PTransformd(Eigen::Vector3d(0, 0, 0.2)) * lfTask_->target());
@@ -130,13 +131,101 @@ struct MC_CONTROL_DLLAPI TestContactsController : public MCController
       auto X_0_rf = robot("jvrc1").frame("RightFoot").position();
       BOOST_CHECK(sva::transformError(X_0_rf, X_0_rf_init).vector().norm() < 1e-4);
     }
+    if(nrIter_ == 2000)
+    {
+      // A classic X-Y-Theta
+      Eigen::Vector6d dof = Eigen::Vector6d::Ones();
+      dof(2) = 0.0;
+      dof(3) = 0.0;
+      dof(4) = 0.0;
+      solver().addContact({"jvrc1", "ground", "LeftFoot", "AllGround", mc_rbdyn::Contact::defaultFriction, dof});
+      lfTask_->reset();
+      lfTask_->target(sva::PTransformd(Eigen::Vector3d(0.1, 0.1, 0)) * sva::PTransformd(sva::RotZ(-M_PI / 4))
+                      * lfTask_->target());
+    }
+    if(nrIter_ >= 2000 && nrIter_ < 2500)
+    {
+      static double prev_error = lfTask_->error().value().norm();
+      double error = lfTask_->error().value().norm();
+      if(nrIter_ > 2050)
+      {
+        BOOST_CHECK(error < prev_error);
+      }
+      prev_error = error;
+      static auto X_0_lf_init = robot("jvrc1").frame("LeftFoot").position();
+      auto X_0_lf = robot("jvrc1").frame("LeftFoot").position();
+      auto X_lfi_lf = X_0_lf * X_0_lf_init.inv();
+      auto lfi_lf_error = sva::transformVelocity(X_lfi_lf).vector();
+      lfi_lf_error.segment(2, 3).setZero();
+      BOOST_CHECK(lfi_lf_error.norm() < 1e-4);
+      static auto X_0_rf_init = robot("jvrc1").frame("RightFoot").position();
+      auto X_0_rf = robot("jvrc1").frame("RightFoot").position();
+      BOOST_CHECK(sva::transformError(X_0_rf, X_0_rf_init).vector().norm() < 1e-4);
+    }
+    if(nrIter_ == 2500)
+    {
+      auto & jvrc2 = loadRobot(robot().module(), "jvrc2");
+      jvrc2.q()->value(qInit_);
+      jvrc2.posW({sva::RotZ(M_PI), Eigen::Vector3d(1.0, 0.0, robot().posW().translation().z())});
+      solver().addConstraint(
+          std::make_shared<mc_solver::KinematicsConstraint>(jvrc2, std::array<double, 3>{0.1, 0.05, 0.5}));
+      auto jvrc2_pt = std::make_shared<mc_tasks::PostureTask>(jvrc2);
+      jvrc2_pt->weight(1.0);
+      jvrc2_pt->stiffness(1.0);
+      solver().addTask(jvrc2_pt);
+      solver().addTask(std::make_shared<mc_tasks::CoMTask>(jvrc2));
+      robot().q()->value(qInit_);
+      robot().forwardKinematics();
+      solver().removeContact({"jvrc1", "ground", "LeftFoot", "AllGround"});
+      solver().removeContact({"jvrc1", "ground", "RightFoot", "AllGround"});
+      solver().addContact({"jvrc1", "ground", "LeftFoot", "AllGround"});
+      solver().addContact({"jvrc1", "ground", "RightFoot", "AllGround"});
+      solver().addContact({"jvrc2", "ground", "LeftFoot", "AllGround"});
+      solver().addContact({"jvrc2", "ground", "RightFoot", "AllGround"});
+      rhTask_ = std::make_shared<mc_tasks::TransformTask>(robot().frame("RightGripper"));
+      rhTask_->stiffness(100.0);
+      auto target = rhTask_->target();
+      target.rotation() = sva::RotY(-M_PI / 2) * sva::RotZ(M_PI);
+      target.translation().x() += 0.25;
+      target.translation().z() += 0.25;
+      rhTask_->target(target);
+      solver().addTask(rhTask_);
+      lhTask_ = std::make_shared<mc_tasks::TransformTask>(robot("jvrc2").frame("LeftGripper"));
+      lhTask_->stiffness(100.0);
+      target.rotation() = sva::RotY(-M_PI / 2);
+      lhTask_->target(target);
+      solver().addTask(lhTask_);
+    }
+    if(nrIter_ == 3000)
+    {
+      solver().addVirtualContact({"jvrc1", "jvrc2", "RightGripper", "LeftGripper"});
+      solver().removeTask(lhTask_);
+      rhTask_->stiffness(10.0);
+    }
+    if(nrIter_ >= 3000 && nrIter_ < 3500)
+    {
+      auto X_0_f1 = robot().frame("RightGripper").position();
+      auto X_0_f2 = robot("jvrc2").frame("LeftGripper").position();
+      static auto X_f1_f2_init = X_0_f2 * X_0_f1.inv();
+      auto X_f1_f2 = X_0_f2 * X_0_f1.inv();
+      auto error = sva::transformError(X_f1_f2_init, X_f1_f2);
+      BOOST_CHECK(error.vector().norm() < 1e-4);
+    }
     return ret;
+  }
+
+  void stop() override
+  {
+    BOOST_REQUIRE(nrIter_ > 3500);
   }
 
 private:
   size_t nrIter_ = 0;
+  Eigen::VectorXd qInit_;
   std::shared_ptr<mc_tasks::CoMTask> comTask_;
   std::shared_ptr<mc_tasks::TransformTask> lfTask_;
+  std::shared_ptr<mc_tasks::TransformTask> rhTask_;
+  std::shared_ptr<mc_tasks::TransformTask> lhTask_;
 };
 
 } // namespace mc_control
